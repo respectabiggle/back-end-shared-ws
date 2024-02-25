cfa95b53c453c3804a9faf9a49a9a97591d7a417
respectabiggle <55676087+respectabiggle@users.noreply.github.com>
2024-02-25 10:23:30 -0500

    formatting

--- app/emit.js
+++ app/emit.js
@@ -1,15 +1,15 @@
 import { log } 					from 'console'
 
 
-import { createServer }			from 'http'
-const httpServer 				= createServer()
-httpServer						.listen(3000)
+import { createServer }	from 'http'
+const httpServer = createServer()
+httpServer.listen(3000)
 
-import { Server } 				from 'socket.io'
-const io		 				= new Server(httpServer)
+import { Server } from 'socket.io'
+const io = new Server(httpServer)
 
-import { WebSocket } 			from 'ws'
-const ws 						= new WebSocket('wss://ws.coincap.io/prices?assets=ethereum')
+import { WebSocket } from 'ws'
+const ws = new WebSocket('wss://ws.coincap.io/prices?assets=ethereum')
 
 
 
